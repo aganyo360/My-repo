15c5af447b6917cda370f672f5e7b92e1e9e9705
aganyo360 <wilsonaganyo@gmail.com.>
2025-01-06 17:01:36 +0300

    anonymous function

---
+++ app.js
@@ -0,0 +1,37 @@
+// let guess = Math.random()*100
+// let computerGuess = Math.round(guess)
+// let humanGuess = Number(prompt("Please Enter your gueess :"));
+
+// if(humanGuess == computerGuess){
+//     alert("you win ")
+// }
+
+// else if(humanGuess>computerGuess){
+//     alert("Too high")
+// }
+// else if(humanGuess<computerGuess){
+//     alert("too low")
+// }
+// else{
+//     alert("Game Cancelled")
+// }
+
+
+// // function winGame(){
+// //     computerGuess == humanGuess
+// //     alert("You won the game")
+// // }
+
+// // function tooLow(){
+// //     if(humanGuess<computerGuess){
+// //         alert("Please Try again too low")
+// //     }
+// // }
+
+
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+
+textBox.addEventListener("keydown", (event) => {
+  output.textContent = `You pressed "${event.key}".`;
+});
