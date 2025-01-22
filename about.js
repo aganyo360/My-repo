eea409feb00e5bc0f2d22727523a9a38ebbb311b
aganyo360 <wilsonaganyo@gmail.com.>
2025-01-22 11:11:21 +0300

    manipulate the DOM

--- about.js
+++ about.js
@@ -1,9 +1,27 @@
-// let paragraphs = document.querySelectorAll("p")
+// // let paragraphs = document.querySelectorAll("p")
 
-let buttons = document.querySelector(".btn")
+// let buttons = document.querySelector(".btn")
 
-function greeting(){
-    alert("Hellow world")
-}
+// function greeting(){
+//     alert("Hellow world")
+// }
 
-buttons.addEventListener('click', greeting)
+// buttons.addEventListener('click', greeting)
+// let first_heading = document.querySelector('#first_heading')
+
+// let content = document.createElement('div');
+// content.classList.add("content")
+// content.textContent = "WE are the world and we are the children"
+// first_heading.appendChild(content)
+
+// let container = document.querySelector('#container')
+// let content = document.createElement('div') 
+// content.textContent = "Code like a pro"
+// content.style.cssText ="color: red; font-size:45px; background-color:yellow"
+// container.appendChild(content)
+
+// let second_child = container.classList.add("second_child")
+// content2 = document.createElement("p")
+// content2.textContent = "This is how we have challenged ourselves and accomplished this"
+// content2.style.cssText = ""
+// second_child.appendChild(content2)
